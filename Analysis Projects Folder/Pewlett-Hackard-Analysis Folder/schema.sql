0ca6962ec469b9ccb025cbcac314a3051f456182
Kelvin Leon <leon.kelvin2000@gmail.com>
2022-04-09 11:46:58 -0400

    updated query

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/schema.sql
@@ -18,12 +18,12 @@ CREATE TABLE employees (
 
 CREATE TABLE dept_manager(
 dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL, 
-FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
-FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
-	PRIMARY KEY(emp_no, dept_no)	
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries(
@@ -37,12 +37,12 @@ CREATE TABLE salaries(
 
 CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
-	from_date INT NOT NULL,
-	to_date INT NOT NULL,
-	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
-	PRIMARY KEY(emp_no,dept_no)
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY(emp_no, dept_no)
 );
 
 CREATE TABLE titles(
@@ -51,7 +51,6 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY(emp_no)
-);
+	PRIMARY KEY (emp_no,title,from_date));
 
-SELECT * FROM departments;
+SELECT * FROM dept_emp;
